47c45a1a536649a650ce4da4422ee4ca9af38993
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-06 14:12:47 +0000

    created what is your name page and new page

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -5,6 +5,10 @@ class BattleShips < Sinatra::Base
     erb :index
   end
 
+  get '/newgame' do
+  	erb :newgame
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
